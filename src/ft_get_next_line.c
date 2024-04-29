eb071c534fb937a452e6957072e48f97d1e3e573
Dima Dem <50800563+dimadem@users.noreply.github.com>
2024-04-29 13:11:12 +0100

    Add files via upload

--- src/ft_get_next_line.c
+++ src/ft_get_next_line.c
@@ -11,102 +11,105 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-#include "get_next_line.h"
 
-void	create_list(t_str_list **list, int fd);
-char	*get_line(t_str_list *list);
-void	check_list(t_str_list **list);
-void	append(t_str_list **list, char *buf, int fd);
+char	*ft_strjoin_gnl(char *s1, char const *s2)
+{
+	char	*str;
+	size_t	s1_len;
+	size_t	s2_len;
+
+	if (!s2)
+		return (NULL);
+	if (s1)
+		s1_len = ft_strlen(s1);
+	else
+		s1_len = 0;
+	s2_len = ft_strlen(s2);
+	str = (char *)malloc(s1_len + s2_len + 1);
+	if (!str)
+		return (NULL);
+	if (s1)
+		ft_strlcpy(str, s1, s1_len + 1);
+	else
+		str[0] = '\0';
+	ft_memcpy(str + s1_len, s2, s2_len + 1);
+	return (str);
+}
+
+static char	*read_from_fd(int fd, char *saved, char *buf)
+{
+	int		chars_read;
+	char	*temp;
+
+	chars_read = read(fd, buf, BUFFER_SIZE);
+	while (chars_read > 0)
+	{
+		buf[chars_read] = '\0';
+		temp = saved;
+		saved = ft_strjoin_gnl(saved, buf);
+		free(temp);
+		if (ft_strchr(saved, '\n'))
+			break ;
+		chars_read = read(fd, buf, BUFFER_SIZE);
+	}
+	free(buf);
+	if (chars_read < 0)
+	{
+		free(saved);
+		saved = NULL;
+		return (NULL);
+	}
+	return (saved);
+}
+
+static char	*extract_line(char **saved)
+{
+	char	*new_line_ptr;
+	char	*line;
+	char	*temp;
+
+	new_line_ptr = ft_strchr(*saved, '\n');
+	if (new_line_ptr)
+	{
+		line = ft_substr(*saved, 0, new_line_ptr - *saved + 1);
+		temp = *saved;
+		*saved = ft_strdup(new_line_ptr + 1);
+		free(temp);
+		return (line);
+	}
+	if (**saved)
+	{
+		line = ft_strdup(*saved);
+		free(*saved);
+		*saved = NULL;
+		return (line);
+	}
+	return (NULL);
+}
 
 char	*ft_get_next_line(int fd)
 {
-	static t_str_list	*list[4096];
-	char				*next_line;
-
-	if (fd < 0 || fd > 4095 || BUFFER_SIZE <= 0 || read(fd, &next_line, 0) < 0)
-		return (NULL);
-	create_list(list, fd);
-	if (list[fd] == NULL)
-		return (NULL);
-	next_line = get_line(list[fd]);
-	check_list(&list[fd]);
-	return (next_line);
-}
-
-void	check_list(t_str_list **list)
-{
-	t_str_list	*last_node;
-	t_str_list	*clean_node;
-	int			i;
-	int			k;
+	static char	*saved = NULL;
+	char		*line;
 	char		*buf;
 
-	buf = (char *)malloc(BUFFER_SIZE + 1);
-	clean_node = malloc(sizeof(t_str_list));
-	if (!clean_node || !buf)
-		return ;
-	last_node = find_last_node(*list);
-	i = 0;
-	k = 0;
-	while (last_node->str_buf[i] && last_node->str_buf[i] != '\n')
-		i++;
-	while (last_node->str_buf[i] && last_node->str_buf[++i])
-		buf[k++] = last_node->str_buf[i];
-	buf[k] = '\0';
-	clean_node->str_buf = buf;
-	clean_node->next = NULL;
-	dealloc(list, clean_node, buf);
-}
-
-char	*get_line(t_str_list *list)
-{
-	int		str_len;
-	char	*next_str;
-
-	if (!list)
+	if (fd < 0 || BUFFER_SIZE < 1 || fd > 4095)
 		return (NULL);
-	str_len = len_to_newline(list);
-	next_str = (char *)malloc(str_len + 1);
-	if (!next_str)
+	buf = malloc(BUFFER_SIZE + 1);
+	if (!buf)
 		return (NULL);
-	copy_str(list, next_str);
-	return (next_str);
-}
-
-void	append(t_str_list **list, char *buf, int fd)
-{
-	t_str_list	*new_node;
-	t_str_list	*last_node;
-
-	last_node = find_last_node(list[fd]);
-	new_node = malloc(sizeof(t_str_list));
-	if (!new_node)
-		return ;
-	if (!last_node)
-		list[fd] = new_node;
-	else
-		last_node->next = new_node;
-	new_node->str_buf = buf;
-	new_node->next = NULL;
-}
-
-void	create_list(t_str_list **list, int fd)
-{
-	int		char_count;
-	char	*buf;
-
-	while (!find_newline(list[fd]))
+	saved = read_from_fd(fd, saved, buf);
+	if (!saved)
 	{
-		buf = malloc(BUFFER_SIZE + 1);
-		if (!buf)
-			return ;
-		char_count = read(fd, buf, BUFFER_SIZE);
-		if (char_count < 0)
-		{
-			free(buf);
-			return ;
-		}
-		buf[char_count] = '\0';
-		append(list, buf, fd);
+		free(saved);
+		saved = NULL;
+		return (NULL);
 	}
+	line = extract_line(&saved);
+	if (line == NULL)
+	{
+		free(saved);
+		saved = NULL;
+	}
+	return (line);
 }
